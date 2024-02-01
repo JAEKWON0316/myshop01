62fd58bbe5db8e763346274fcde1587eb5c9f30d
JK Lee <diwo02@naver.com>
2024-02-01 15:25:10 +0900

    card-body 작업중

--- js/db.js
+++ js/db.js
@@ -1,6 +1,6 @@
 $(function(){
     $.ajax({
-        url: "./data/db.json", //데이터 받아올 경로 지정
+        url: "./data/db.json", //데이터 받아올 경로 지정 (html에서 부터 시작).
         success: function(rs){ //rs라는 매개변수로 데이터를 받아옴
             const cat = rs.category;
             let link='';
@@ -21,6 +21,46 @@ $(function(){
                }
             }
             $('.pr-category').html(link);
+            $('.category-subnav-view').html(link);
          }
        });
+
+       $.get("data/slide.json", function(rs){
+         const img = rs.carousel;
+         let imgs = ''; //배열에 맞는 값을 넣어줄 공백변수 imgs생성
+         for(let i = 0; i < img.length; i++){
+            imgs += `<div class="text-center"><img src="images/${img[i]}" alt="${img[i]}"></div>`;
+         }
+         $('.mySlick').prepend(imgs).slick({//밑에와 똑같다!(단, 안에 내용이 없을 떄만!)
+            // $('.mySlick').html(imgs);
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            autoplay: true,
+            autoplaySpeed: 5000 
+         });
+       });
+
+       $.get("data/new.json", function(rs){
+            let newbox = '';
+            const list = rs.newlist;
+            for(let i = 0; i < list.length; i++){
+               newbox += `
+                  <div class="col-md-3">
+                     <div class="card">
+                           <a href="#" class="card-img"><img src="${list[i].img}" class="card-img-top" alt="nothing">
+                           </a>
+                           <a href="#" class="card-body">
+                              <h5 class="mt-4">${list[i].title}</h5>
+                              <p>${list[i].txt}</p>
+                           </a>
+                     </div>
+                  </div>
+               `
+            };
+            $('.container .card-array').html(newbox);
+       });
+
  }); //jquery
--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 $(function(){
     $(document)
     .on("mouseover", '.pr-category>li', function(){  //sub-cate클래스는 html에서 만든것이 아닌 js에서 따로 만든것이기 때문에 document.on 해서 찾아줘야한다.
-        $(this).find('.sub-cate').fadeIn(50);
+        $(this).find('.sub-cate').fadeIn(10);
     })
     .on("mouseleave", '.pr-category>li', function(){
-        $('.sub-cate').fadeOut(50);
+        $('.pr-category>li>.sub-cate').fadeOut(10); //서열을 맥여서 pr-categoty하위에 있는 sub-cate만 안보이게 한다->밑에서 또 sub-cate를 쓰기 때문에 그건 사라지면 안되니까!!
+    });
+
+    $(".category").hover(function(){
+        $(this).find('.category-subnav').css('display', 'flex');
+    }).mouseleave(function(){
+        $(this).find('.category-subnav').css('display', 'none');
     });
     // $('.pr-category>li').hover(function(){
     //     $(this).find('.sub-cate').fadeToggle();
