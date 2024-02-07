6b2f45adf2412338bb3364ff24e44670b6504f61
JK Lee <diwo02@naver.com>
2024-02-07 12:29:31 +0900

    모바일 작업중

--- js/detail.js
+++ js/detail.js
@@ -9,4 +9,38 @@ $(function(){
         $('.selected').text(color+"색");
     });
     
+    //상품가격 계산
+    const prcode = $("#prcode").val();
+    const prprice = Number($('#prprice').val());
+    const reserves = parseInt($("#reserves").val());
+    const delivery = parseInt($("#delivery").val());
+    let totalmoney = prprice;
+    
+    $("#qup").click(function(){
+        let quantity = Number($('#quantity').val()); //Number숫자값으로 받아줘야함
+        quantity += 1;
+        if(quantity > 9){
+            alert("최대수량입니다.");
+            quantity = 9;
+        }
+        $('#quantity').val(quantity);
+        totalmoney = prprice * quantity;
+        let tmoney = totalmoney.toLocaleString();
+        let txt = "총 상품금액(수량) : <strong>"+tmoney+"원<strong>("+quantity+"개)";
+        $('.totalmoney').html(txt);        
+        //$('.totalmoney>strong').text(totalmoney.toLocaleString()+"원");
+    });
+    $("#qdown").click(function(){
+        let quantity = Number($('#quantity').val()); //Number숫자값으로 받아줘야함
+        quantity -= 1;
+        if(quantity < 1){
+            quantity = 1;
+        }
+        $('#quantity').val(quantity);
+        totalmoney = prprice * quantity;
+        let tmoney = totalmoney.toLocaleString();
+        let txt = "총 상품금액(수량) : <strong>"+tmoney+"원<strong>("+quantity+"개)";
+        $('.totalmoney').html(txt);        
+        //$('.totalmoney>strong').text(totalmoney.toLocaleString()+"원");
+    });
 }); //jquery
--- js/script.js
+++ js/script.js
@@ -21,18 +21,21 @@ $(function(){
         }
      });
     $(document)
-    .on("mouseover", '.pr-category>li', function(){  //sub-cate클래스는 html에서 만든것이 아닌 js에서 따로 만든것이기 때문에 document.on 해서 찾아줘야한다.
+    .on("mouseenter", '.pr-category>li', function(){  //sub-cate클래스는 html에서 만든것이 아닌 js에서 따로 만든것이기 때문에 document.on 해서 찾아줘야한다.
         $(this).find('.sub-cate').fadeIn(10);
     })
     .on("mouseleave", '.pr-category>li', function(){
         $('.pr-category>li>.sub-cate').fadeOut(10); //서열을 맥여서 pr-categoty하위에 있는 sub-cate만 안보이게 한다->밑에서 또 sub-cate를 쓰기 때문에 그건 사라지면 안되니까!!
     });
 
-    $(".category").hover(function(){
-        $(this).find('.category-subnav').css('display', 'flex');
-    }).mouseleave(function(){
-        $(this).find('.category-subnav').css('display', 'none');
-    });
+    $(".category").click(function(){
+        if($(this).find('.category-subnav').css('display') == 'none'){
+            $(this).find('.category-subnav').css('display', 'flex');
+        }
+        else{
+            $(this).find('.category-subnav').css('display', 'none');
+        }
+});
     // $('.pr-category>li').hover(function(){
     //     $(this).find('.sub-cate').fadeToggle();
     // });
