9f1ae412ee80570da9ef0b0d67d821a4f8f84468
JK Lee <diwo02@naver.com>
2024-02-14 14:04:19 +0900

    작업중

--- js/detail.js
+++ js/detail.js
@@ -16,62 +16,79 @@ $(function(){
     const delivery = parseInt($("#delivery").val());
     const prtitle = $("#title").val();
     let totalmoney = prprice;
-    let opt1, colortxt, color, opt2, size, sizetxt, optionText; //외부에서도 선언해줘야지 다른 함수에서도 사용할 수 있으니까!!(함수내에 변수를 설정하면 그 함수내(스코프)안에서 밖에 못쓴다!!)
+    let tmoney = prprice;
+    let quantity, totalTextLength, opt1, opt11, colortxt, color, opt2, opt21, size, sizetxt, optionText; //외부에서도 선언해줘야지 다른 함수에서도 사용할 수 있으니까!!(함수내에 변수를 설정하면 그 함수내(스코프)안에서 밖에 못쓴다!!)
    
     $('input[name="color"]').change(function(){
         //alert(this.value);
+        $('.size').find("option:first").prop("selected", true)
        opt1 = $(this).data('color'); //data-color의 값을 받아온다(추가금액)
-       colortxt = $(this).data('colorname'); //input타입중 name이 color인것의 value를 받아온다.
-       color = $(this).val();
+       colortxt = $(this).data('colorname'); //input타입중 name이 color인것의 value를 받아온다.(화면에 출력할 생삭이름)
+       color = $(this).val();  //폼으로 전송할 색상값
        if(opt1 > 0){
-        opt1 = "(+" + opt1.toLocaleString()+ "원)";
+        opt11 = "(+" + opt1.toLocaleString()+ "원)";
     }else{
-        opt1 = "";
+        opt11 = "";
     }
-        colortxt += " " + opt1; 
+        colortxt += " " + opt11;   //화면에 출력할 색상이름
+        $('.size').attr('disabled', false); //size클래스의 disabled 속성을 change시 false하게 한다
     });
-    let opthtml = ` <ul class="add-opt">
-                <li class="total-text">
-                    
-                </li>
-                <li class="addbox d-flex align-items-center">
-                    <label class="title-label">수량</label>
-                    <div class="input-group mb-3 ml-4">
-                    <div class="input-group-prepend">
-                        <button class="btn btn-outline-line" id="qdown" type="button">
-                            <i class="fa-solid fa-chevron-down"></i>
-                        </button>
-                    </div>
-                    <input type="number" class="quantity" id="quantity" name="quantity" value="1" readonly> <!--readonly => 글씨로 수정불가능-->
-                    <div class="input-group-append">
-                        <button class="btn btn-outline-line" id="qup" type="button">
-                            <i class="fa-solid fa-chevron-up"></i>
-                        </button>
-                    </div>
-                </div>
-                </li>
-                <li class="tomoney">
-                    
-                </li>
-            </ul>`
+    let opthtml = `<ul class="add-opt">
+                     <li class="d-flex align-items-center">
+                        <div class="total-text col"></div>
+                         <ul class="add-opts col">
+                              <li class="addbox d-flex align-items-center">
+                             <label class="title-label">수량</label>
+                              <div class="input-group">
+                                  <div class="input-group-prepend">
+                                       <button class="btn btn-outline-line qdown" type="button">
+                                       <i class="fa-solid fa-chevron-down"></i>
+                                     </button>
+                                   </div>
+                                  <input type="number" 
+                                        class="quantity" 
+                                        name="quantity" 
+                                        value="1"
+                                        readonly>
+                                  <div class="input-group-append">
+                                     <button class="btn btn-outline-line qup" type="button">
+                                         <i class="fa-solid fa-chevron-up"></i>
+                                    </button>
+                                  </div> 
+                             </div>
+                         </li>
+                    </ul>
+                    <div class="tomoney col text-right"></div>
+                    <i class="fa-solid fa-close remove-order"></i>
+                    </li>
+
+            </ul>`;
         let i = 0;
         let textArray = [];
     $('.size').change(function(){
         //alert(this.value);
+        const totalTextLength = $('.total-text').length;
+        const oradd = $('.addquantity').html();
+        console.log(oradd);
         opt2 = $(this).find("option:selected").data('size'); //사이즈별 추가금액(라디오 박스에서! 옵션이 선택된 애들의 데이터를) 
         size = $(this).find("option:selected").val(); 
         sizetxt = $(this).find("option:selected").text();
         if(opt2 > 0){
-            opt2 = "(+" + opt2.toLocaleString()+ "원)";
+            
+            opt21 = "(+" + opt2.toLocaleString()+ "원)";
         }
         else{
-            opt2 = "";
+            opt21 = "";
         }
-        sizetxt += " " + opt2; 
-        optionText = `<p>${prtitle}, ${colortxt}-${sizetxt}</p>`;
-        const oradd = $('.addquantity').html();
-        $(".addquantity").html(oradd + opthtml);
-        $('.total-text').html(optionText);
+        if(size){
+        tmoney = (prprice + (opt1 + opt2));
+        sizetxt += " " + opt21; 
+        optionText = `<p>${colortxt}-${sizetxt}</p>`;
+        
+        $(".addquantity").html(oradd + opthtml); //기존거 + 새로운거
+        $('.total-text').eq(totalTextLength).html(optionText); //eq == 순서를 나타냄(totalTextLength로) 0,1,2,3,...
+        $('.tomoney').eq(totalTextLength).html(tmoney.toLocaleString()+"원");
+       }
         // textArray = $('.total-text').html(optionText); 
         // console.log(textArray[i]);
         
@@ -80,15 +97,17 @@ $(function(){
 
    
     //$("#qup").click(function(){
-        $(document).on('click', '#qup', function(){  //html에 있는걸 뺴고 js로 만들어줬으니 document로 부터 받아와야한다! $(document).on()
-
-        let quantity = Number($('#quantity').val()); //Number숫자값으로 받아줘야함
+        $(document).on('click', '.qup', function(){  //html에 있는걸 뺴고 js로 만들어줬으니 document로 부터 받아와야한다! $(document).on()
+        let quantity = Number($(this).parent().prev().val());
+        //let quantity = Number($('.quantity').eq(totalTextLength).val()); //Number숫자값으로 받아줘야함
         quantity += 1;
         if(quantity > 9){
             alert("최대수량입니다.");
             quantity = 9;
         }
-        $('#quantity').val(quantity);
+        console.log(quantity);
+        //$('.quantity').eq(totalTextLength).val(quantity);
+        $(this).parent().prev().val(quantity); //누른것의 부모의 형제의 이전에 있는 값을 받아온다 (올라가는값);
         totalmoney = prprice * quantity;
         let tmoney = totalmoney.toLocaleString();
         let txt = "총 상품금액(수량) : <strong>"+tmoney+"원<strong>("+quantity+"개)";
@@ -96,17 +115,25 @@ $(function(){
         //$('.totalmoney>strong').text(totalmoney.toLocaleString()+"원");
     });
     //$("#qdown").click(function(){
-        $(document).on('click', '#qdown', function(){ //html에 있는걸 뺴고 js로 만들어줬으니 document로 부터 받아와야한다!
-        let quantity = Number($('#quantity').val()); //Number숫자값으로 받아줘야함
+        $(document).on('click', '.qdown', function(){ //html에 있는걸 뺴고 js로 만들어줬으니 document로 부터 받아와야한다!
+        //let quantity = Number($('.quantity').eq(totalTextLength).val()); //Number숫자값으로 받아줘야함
+        let quantity = Number($(this).parent().next().val());
         quantity -= 1;
         if(quantity < 1){
             quantity = 1;
         }
-        $('#quantity').val(quantity);
+        //$('.quantity').eq(totalTextLength).val(quantity);
+        $(this).parent().next().val(quantity); //누른것의 부모의 형제의 다음에 있는 값을 받아온다 (내려가는값);
         totalmoney = prprice * quantity;
         let tmoney = totalmoney.toLocaleString();
         let txt = "총 상품금액(수량) : <strong>"+tmoney+"원<strong>("+quantity+"개)";
         $('.totalmoney').html(txt);        
         //$('.totalmoney>strong').text(totalmoney.toLocaleString()+"원");
+        
     });
+
+    $(document).on('click', '.remove-order', function(){
+        $(this).parents('.add-opt').remove(); //클릭한것의 부모들중 add-opt클래스를 찾아서 삭제해라.
+    });
+
 }); //jquery
