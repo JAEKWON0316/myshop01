96094b06637cb54e218203fb7f860e413daf6868
JK Lee <diwo02@naver.com>
2024-02-21 13:19:33 +0900

    works 작업중입니당

---
+++ js/cart.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.hot-photo').slick({
+      
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        centerMode: true
+      });
+ 
+
+}); //jquery
